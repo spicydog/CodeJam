6ccbee190ffaeb56d36072447866d188ac868464
Spicydog Proxy <spicydog@gmail.com>
2015-04-12 18:05:06 +0700

    Optimize 2015 C for large data set

--- 2015/Qualification Round/Problem C. Dijkstra/c.php
+++ 2015/Qualification Round/Problem C. Dijkstra/c.php
@@ -1,5 +1,6 @@
 <?php
 
+
 define('FILENAME','C-large');
 
 
@@ -36,6 +37,10 @@ for ($i=1; $i <= $nLine*2; $i++) {
   $repeat = $row[1];
   $str = $lines[++$i];
 
+
+  if(($i/2)<73) {
+    // continue;
+  }
   echo $print = 'Case #'.($i/2).': ' . (isDijkstra($str,$repeat)? 'YES' : 'NO') . "\n";
   $output .= $print;
 
@@ -52,35 +57,43 @@ function isDijkstra($str,$repeat) {
 
   $lastResult = '';
   $sameResultCount = 0;
+  $results[0] = '';
   for($i=1;$i<=$repeat;$i++) {
 
     $result = getDijkstra($str,$i);
+
+    $results[$i] = $result;
 
     $isFound = false;
     if($result=='ijk') {
       $isFound = true;
     }
 
+    // Fing pattern
     if($isFound) {
       if($init==0) {
         $init = $i;
       } else {
+        // Found a pattern
         $diff = $i-$init;
         break;
       }
     }
 
-    // TODO need to check sequence of result for large data set
-    if($lastResult==$result) {
-      $sameResultCount++;
-      if($sameResultCount>3) {
-        break;
-      }
-    } else {
-      $lastResult = $result;
-      $sameResultCount = 0;
+    // Find repeat result
+    if(getLastRepeatingArray($results)>5) {
+      break;
     }
 
+    // Find repeat pattern
+    // we will check lately because it consume so much CPU,
+    // and it will return better result when the string if long enough
+    if($i>250) {
+      $largestRepeatCount = getLargestRepeatingStringCount($results);
+      if($largestRepeatCount>5) {
+        break;
+      }
+    }
 
   }
 
@@ -96,8 +109,59 @@ function isDijkstra($str,$repeat) {
   }
 }
 
+function getLastRepeatingArray($results) {
+  $lastIndex = count($results)-1;
+  $lastestResult = $results[$lastIndex];
+  $countRepeat = 0;
+  for($i=$lastIndex;$i>=0;$i--) {
+    if($lastestResult == $results[$i]) {
+      $countRepeat++;
+    } else {
+      break;
+    }
+  }
+  return $countRepeat;
+}
 
+function getLargestRepeatingStringCount($words) {
 
+  $str = implode($words);
+  $results = getRepeatStrings($str);
+  $max = 0;
+  if(isset(array_values($results)[0])) {
+    $max = array_values($results)[0];
+  }
+  return $max;
+}
+
+// This function is super useful but consume high CPU power
+function getRepeatStrings($str) {
+
+  $results = [];
+  for($i=strlen($str)-1;$i>0;$i--) {
+
+    $substring = substr($str,$i);
+
+    $pass = true;
+    $left = $str;
+    $count = 0;
+    while($pass) {
+      $checkSubstring = substr($left,strlen($left)-strlen($substring));
+      if($checkSubstring==$substring) {
+        $left = substr($left,0,-strlen($substring));
+        $count++;
+      } else {
+        $pass = false;
+      }
+    }
+
+    $results[$substring] = $count;
+  }
+
+  arsort($results);
+
+  return $results;
+}
 
 function getDijkstra($str,$repeat) {
 
@@ -164,6 +228,5 @@ function mixChar($x,$y) {
   return $table[$x][$y];
 
 }
-
 
 ?>
